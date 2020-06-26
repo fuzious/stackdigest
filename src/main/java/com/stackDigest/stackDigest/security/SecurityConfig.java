42e16d1da0de839886424946ae77d5125356e320
fuzious <arpitsrivastava2012@gmail.com>
2020-06-27 00:55:35 +0530

    v1.0 published

--- src/main/java/com/stackDigest/stackDigest/security/SecurityConfig.java
+++ src/main/java/com/stackDigest/stackDigest/security/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable()
-				.authorizeRequests().antMatchers("/","/feedData/**","/feed","/js/**","/css/**","/images/**","/resources/**","/oauth/**","/registerUser","/login").permitAll()
+				.authorizeRequests().antMatchers("/","/js/**","/css/**","/images/**","/resources/**","/oauth/**","/registerUser","/login").permitAll()
 				.anyRequest().authenticated()
 				.and()
 				.formLogin().loginPage("/login").defaultSuccessUrl("/loggedin").permitAll()
@@ -42,6 +42,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.logout().invalidateHttpSession(true)
 				.clearAuthentication(true)
 				.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-				.logoutSuccessUrl("/logout-success").permitAll();
+				.logoutSuccessUrl("/").permitAll();
 	}
 }
